14323d3d2fd8799fbe00e254a107ebe7c07a3840
teamzz111 <felipeflogxd@gmail.com>
2022-02-04 21:13:22 -0500

    remove margin

--- lib/card.dart
+++ lib/card.dart
@@ -10,7 +10,7 @@ class CardDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
+      margin: const EdgeInsets.only(top: 10, left: 15, right: 15),
       padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
         color: Colors.white,
